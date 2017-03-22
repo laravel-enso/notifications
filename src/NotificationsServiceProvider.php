18383a0b2601a742935d1d194c371cf569391320
mishu <mishu@xtelecom.ro>
2017-03-22 13:42:31 +0200

    fixed for laravel-enso/core

--- src/NotificationsServiceProvider.php
+++ src/NotificationsServiceProvider.php
@@ -24,7 +24,7 @@ class NotificationsServiceProvider extends ServiceProvider
         ], 'notifications-migration');
 
         $this->publishes([
-            __DIR__.'/../resources/assets/js/components' => resource_path('assets/js/components/laravel-enso'),
+            __DIR__.'/../resources/assets/js/components' => resource_path('assets/js/vendor/laravel-enso/components'),
         ], 'notifications-component');
 
         $this->publishes([
