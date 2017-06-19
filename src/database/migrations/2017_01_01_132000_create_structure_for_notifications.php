c7048a4d0652e2b2f8cd4923d3f017a3628b8569
mishu <mishu@xtelecom.ro>
2017-06-19 17:55:46 +0300

    updated structure creator with default attribute for permissions

--- src/database/migrations/2017_01_01_132000_create_structure_for_notifications.php
+++ src/database/migrations/2017_01_01_132000_create_structure_for_notifications.php
@@ -9,10 +9,10 @@ class CreateStructureForNotifications extends StructureMigration
     ];
 
     protected $permissions = [
-        ['name' => 'core.notifications.getCount', 'description' => 'Get Users Notifications Count', 'type' => 1],
-        ['name' => 'core.notifications.getList', 'description' => 'Get Users Notifications List', 'type' => 0],
-        ['name' => 'core.notifications.markAsRead', 'description' => 'Mark notification as Read', 'type' => 1],
-        ['name' => 'core.notifications.markAllAsRead', 'description' => 'Marl All notifications as Read', 'type' => 1],
-        ['name' => 'core.notifications.clearAll', 'description' => 'Clear all notifications', 'type' => 1],
+        ['name' => 'core.notifications.getCount', 'description' => 'Get Users Notifications Count', 'type' => 1, 'default' => true],
+        ['name' => 'core.notifications.getList', 'description' => 'Get Users Notifications List', 'type' => 0, 'default' => true],
+        ['name' => 'core.notifications.markAsRead', 'description' => 'Mark notification as Read', 'type' => 1, 'default' => true],
+        ['name' => 'core.notifications.markAllAsRead', 'description' => 'Marl All notifications as Read', 'type' => 1, 'default' => true],
+        ['name' => 'core.notifications.clearAll', 'description' => 'Clear all notifications', 'type' => 1, 'default' => true]
     ];
 }
