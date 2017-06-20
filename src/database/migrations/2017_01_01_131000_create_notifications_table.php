bc913497b1f15636b09a2dced4bb93a15420e9d9
mishu <mishu@xtelecom.ro>
2017-06-20 21:07:10 +0300

    fix type in structure creator

--- src/database/migrations/2017_01_01_131000_create_notifications_table.php
+++ src/database/migrations/2017_01_01_131000_create_notifications_table.php
@@ -5,11 +5,6 @@ use Illuminate\Database\Schema\Blueprint;
 
 class CreateNotificationsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('notifications', function (Blueprint $table) {
@@ -22,11 +17,6 @@ class CreateNotificationsTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::drop('notifications');
--- src/database/migrations/2017_01_01_132000_create_structure_for_notifications.php
+++ src/database/migrations/2017_01_01_132000_create_structure_for_notifications.php
@@ -12,7 +12,7 @@ class CreateStructureForNotifications extends StructureMigration
         ['name' => 'core.notifications.getCount', 'description' => 'Get Users Notifications Count', 'type' => 1, 'default' => true],
         ['name' => 'core.notifications.getList', 'description' => 'Get Users Notifications List', 'type' => 0, 'default' => true],
         ['name' => 'core.notifications.markAsRead', 'description' => 'Mark notification as Read', 'type' => 1, 'default' => true],
-        ['name' => 'core.notifications.markAllAsRead', 'description' => 'Marl All notifications as Read', 'type' => 1, 'default' => true],
+        ['name' => 'core.notifications.markAllAsRead', 'description' => 'Mark All notifications as Read', 'type' => 1, 'default' => true],
         ['name' => 'core.notifications.clearAll', 'description' => 'Clear all notifications', 'type' => 1, 'default' => true],
     ];
 }
