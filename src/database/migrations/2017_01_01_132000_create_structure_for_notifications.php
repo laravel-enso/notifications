b0a4c7183fb1f85aa99e7862950913559e7c5e3a
mishu <mishu@xtelecom.ro>
2017-06-23 14:16:38 +0300

    updated structure migration

--- src/database/migrations/2017_01_01_132000_create_structure_for_notifications.php
+++ src/database/migrations/2017_01_01_132000_create_structure_for_notifications.php
@@ -5,14 +5,14 @@ use LaravelEnso\Core\app\Classes\StructureManager\StructureMigration;
 class CreateStructureForNotifications extends StructureMigration
 {
     protected $permissionGroup = [
-        'name' => 'core.notifications', 'description' => 'Notifications Permissions Group',
+        'name' => 'core.notifications', 'description' => 'Notifications permissions group',
     ];
 
     protected $permissions = [
-        ['name' => 'core.notifications.getCount', 'description' => 'Get Users Notifications Count', 'type' => 1, 'default' => true],
-        ['name' => 'core.notifications.getList', 'description' => 'Get Users Notifications List', 'type' => 0, 'default' => true],
-        ['name' => 'core.notifications.markAsRead', 'description' => 'Mark notification as Read', 'type' => 1, 'default' => true],
-        ['name' => 'core.notifications.markAllAsRead', 'description' => 'Mark All notifications as Read', 'type' => 1, 'default' => true],
+        ['name' => 'core.notifications.getCount', 'description' => 'Get users notifications count', 'type' => 1, 'default' => true],
+        ['name' => 'core.notifications.getList', 'description' => 'Get users notifications list', 'type' => 0, 'default' => true],
+        ['name' => 'core.notifications.markAsRead', 'description' => 'Mark notification as read', 'type' => 1, 'default' => true],
+        ['name' => 'core.notifications.markAllAsRead', 'description' => 'Mark all notifications as read', 'type' => 1, 'default' => true],
         ['name' => 'core.notifications.clearAll', 'description' => 'Clear all notifications', 'type' => 1, 'default' => true],
     ];
 }
