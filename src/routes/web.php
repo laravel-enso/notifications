6cedc1cb8496becde9ee5dd68ebc39921719ced0
mishu <mishu@xtelecom.ro>
2017-07-12 15:43:04 +0300

    updated composer.json for package auto discovery. refactor routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,14 +1,19 @@
 <?php
 
-Route::group([
-    'namespace'  => 'LaravelEnso\Notifications\app\Http\Controllers',
-    'middleware' => ['web', 'auth', 'core'],
-], function () {
-    Route::group(['prefix' => 'core/notifications', 'as' => 'core.notifications.'], function () {
-        Route::get('getCount', 'NotificationController@getCount')->name('getCount');
-        Route::get('getList/{offset}/{paginate}', 'NotificationController@getList')->name('getList');
-        Route::patch('markAsRead/{notification}', 'NotificationController@markAsRead')->name('markAsRead');
-        Route::patch('markAllAsRead', 'NotificationController@markAllAsRead')->name('markAllAsRead');
-        Route::patch('clearAll', 'NotificationController@clearAll')->name('clearAll');
+Route::middleware(['web', 'auth', 'core'])
+    ->namespace('LaravelEnso\Notifications\app\Http\Controllers')
+    ->group(function () {
+        Route::prefix('core/notifications')->as('core.notifications.')
+            ->group(function () {
+                Route::get('getCount', 'NotificationController@getCount')
+                	->name('getCount');
+                Route::get('getList/{offset}/{paginate}', 'NotificationController@getList')
+                	->name('getList');
+                Route::patch('markAsRead/{notification}', 'NotificationController@markAsRead')
+                	->name('markAsRead');
+                Route::patch('markAllAsRead', 'NotificationController@markAllAsRead')
+                	->name('markAllAsRead');
+                Route::patch('clearAll', 'NotificationController@clearAll')
+                	->name('clearAll');
+            });
     });
-});
