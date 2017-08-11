fd26ad225964ef527500aa36ebda0689482e7ac3
mishu <mishu@xtelecom.ro>
2017-08-11 23:30:26 +0300

    moved the component to vueadminlte

--- src/NotificationsServiceProvider.php
+++ src/NotificationsServiceProvider.php
@@ -8,23 +8,6 @@ class NotificationsServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->publishesAll();
-        $this->loadDependencies();
-    }
-
-    private function publishesAll()
-    {
-        $this->publishes([
-            __DIR__.'/resources/assets/js/components' => resource_path('assets/js/vendor/laravel-enso/components'),
-        ], 'notifications-component');
-
-        $this->publishes([
-            __DIR__.'/resources/assets/js/components' => resource_path('assets/js/vendor/laravel-enso/components'),
-        ], 'enso-update');
-    }
-
-    public function loadDependencies()
-    {
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
         $this->loadViewsFrom(__DIR__.'/resources/views', 'laravel-enso/notifications');
