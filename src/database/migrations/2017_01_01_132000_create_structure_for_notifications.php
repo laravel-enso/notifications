bd81980a699d8a6c15e5cf4a78218908e6d70f22
mishu <mishu@xtelecom.ro>
2018-05-28 13:26:25 +0300

    updates for the new structure manager

--- src/database/migrations/2017_01_01_132000_create_structure_for_notifications.php
+++ src/database/migrations/2017_01_01_132000_create_structure_for_notifications.php
@@ -9,10 +9,10 @@ class CreateStructureForNotifications extends StructureMigration
     ];
 
     protected $permissions = [
-        ['name' => 'core.notifications.getCount', 'description' => 'Get users notifications count', 'type' => 1, 'default' => true],
-        ['name' => 'core.notifications.getList', 'description' => 'Get users notifications list', 'type' => 0, 'default' => true],
-        ['name' => 'core.notifications.markAsRead', 'description' => 'Mark notification as read', 'type' => 1, 'default' => true],
-        ['name' => 'core.notifications.markAllAsRead', 'description' => 'Mark all notifications as read', 'type' => 1, 'default' => true],
-        ['name' => 'core.notifications.clearAll', 'description' => 'Clear all notifications', 'type' => 1, 'default' => true],
+        ['name' => 'core.notifications.getCount', 'description' => 'Get users notifications count', 'type' => 1, 'is_default' => true],
+        ['name' => 'core.notifications.getList', 'description' => 'Get users notifications list', 'type' => 0, 'is_default' => true],
+        ['name' => 'core.notifications.markAsRead', 'description' => 'Mark notification as read', 'type' => 1, 'is_default' => true],
+        ['name' => 'core.notifications.markAllAsRead', 'description' => 'Mark all notifications as read', 'type' => 1, 'is_default' => true],
+        ['name' => 'core.notifications.clearAll', 'description' => 'Clear all notifications', 'type' => 1, 'is_default' => true],
     ];
 }
