ef20505303771b39233cc62786e2187b51c01770
mishu <mishu@xtelecom.ro>
2018-10-21 14:54:02 +0300

    removes deprecated command

--- src/AppServiceProvider.php
+++ src/AppServiceProvider.php
@@ -3,29 +3,25 @@
 namespace LaravelEnso\Notifications;
 
 use Illuminate\Support\ServiceProvider;
-use LaravelEnso\Notifications\app\Commands\AddMissingPermissions;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->commands([
-            AddMissingPermissions::class,
-        ]);
-
-        $this->load();
-
-        $this->publish();
+        $this->loadDependencies()
+            ->publishDependencies();
     }
 
-    public function load()
+    public function loadDependencies()
     {
         $this->loadRoutesFrom(__DIR__.'/routes/api.php');
 
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+
+        return $this;
     }
 
-    public function publish()
+    public function publishDependencies()
     {
         $this->publishes([
             __DIR__.'/resources/js' => resource_path('js'),
