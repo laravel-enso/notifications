9392291d9cff1717ee88ba4895415b9dafedd40b
mishu <mishu@xtelecom.ro>
2020-03-14 18:33:24 +0200

    removes Types from migration

--- src/database/migrations/2017_01_01_132000_create_structure_for_notifications.php
+++ src/database/migrations/2017_01_01_132000_create_structure_for_notifications.php
@@ -1,16 +1,15 @@
 <?php
 
 use LaravelEnso\Migrator\App\Database\Migration;
-use LaravelEnso\Permissions\App\Enums\Types;
 
 class CreateStructureForNotifications extends Migration
 {
     protected $permissions = [
-        ['name' => 'core.notifications.index', 'description' => 'Notifications index', 'type' => Types::Read, 'is_default' => true],
-        ['name' => 'core.notifications.count', 'description' => 'Get users notifications count', 'type' => Types::Write, 'is_default' => true],
-        ['name' => 'core.notifications.read', 'description' => 'Mark notification as read', 'type' => Types::Write, 'is_default' => true],
-        ['name' => 'core.notifications.readAll', 'description' => 'Mark all notifications as read', 'type' => Types::Write, 'is_default' => true],
-        ['name' => 'core.notifications.destroy', 'description' => 'Clear a notification', 'type' => Types::Write, 'is_default' => true],
-        ['name' => 'core.notifications.destroyAll', 'description' => 'Clear all notifications', 'type' => Types::Write, 'is_default' => true],
+        ['name' => 'core.notifications.index', 'description' => 'Notifications index', 'is_default' => true],
+        ['name' => 'core.notifications.count', 'description' => 'Get users notifications count', 'is_default' => true],
+        ['name' => 'core.notifications.read', 'description' => 'Mark notification as read', 'is_default' => true],
+        ['name' => 'core.notifications.readAll', 'description' => 'Mark all notifications as read', 'is_default' => true],
+        ['name' => 'core.notifications.destroy', 'description' => 'Clear a notification', 'is_default' => true],
+        ['name' => 'core.notifications.destroyAll', 'description' => 'Clear all notifications', 'is_default' => true],
     ];
 }
